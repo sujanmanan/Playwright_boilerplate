f73d31347029b0d989086b36b8b8a83c1ca06eb4
SujanManandhar <sujanmanandhar@lftechnology.com>
2025-08-28 14:57:19 +0545

    remove unwanted files

--- tests/test-db.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-import { getDataFromDB } from '../Utils/Database';
-
-test('validate db data', async ({ page }) => {
-  let getUser = "select * from users";
-  const client = await getDataFromDB(getUser);  
-});
