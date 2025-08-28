9b0daa3456a6d763252743542cf8ed83c409ec80
SujanManandhar <sujanmanandhar@lftechnology.com>
2025-08-28 14:11:52 +0545

    updates auth page

--- auth.setup.ts
+++ auth.setup.ts
@@ -1,7 +1,7 @@
 import { chromium,FullConfig } from "@playwright/test";
-import { pageUrlEnum } from "./enums/PageURLEnum";
+import { PageUrlEnum } from "./enums/PageUrlEnum";
 import { LoginPage } from "./PageService/LoginPage";
-import { DefaultPage } from "./PageService/DefaultPage";
+import { DefaultPage } from "./Utils/DefaultPage";
 import dotenv from 'dotenv';
 import { PageTitleEnum } from "./enums/PageTitleEnum";
 
@@ -12,7 +12,7 @@ async function globalSetup(config: FullConfig) {
     const page = await browser.newPage();
     const loginPage = new LoginPage(page);
     const defaultPage = new DefaultPage(page);
-    await page.goto(pageUrlEnum.Login);
+    await page.goto(PageUrlEnum.LoginPage);
     defaultPage.shouldBeEqual(await page.title(),PageTitleEnum.Login);
     await loginPage.login(process.env.Test_Username!,process.env.Test_Password!)
     await page.context().storageState({ path: 'auth.json' });
