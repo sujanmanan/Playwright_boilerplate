d7688845ee2734576765d205434c72f2309bd25d
SujanManandhar <sujanmanandhar@lftechnology.com>
2025-08-28 14:50:27 +0545

    remove unused file

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[
     ['html'],
-    // ['./Utils/EmailReporter.ts']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
